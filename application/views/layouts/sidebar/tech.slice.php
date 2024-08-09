2b1d6ecabb1721cf12827b4d8dbaf288d5fea840
syauqyitho <syauqy.devithogny@gmail.com>
2024-08-09 15:12:55 +0700

    add home to sidebar

--- application/views/layouts/sidebar/tech.slice.php
+++ application/views/layouts/sidebar/tech.slice.php
@@ -1,6 +1,6 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?= base_url('welcome/welcome') ?>" class="brand-link">
+    <a href="<?= base_url('tech/dashboard') ?>" class="brand-link">
       <img src="<?= base_url('assets/dist/img/AdminLTELogo.png') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
@@ -13,6 +13,14 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="<?= base_url('tech/dashboard') ?>" class="nav-link">
+              <i class="nav-icon fas fa-home"></i>
+              <p>
+                Beranda
+              </p>
+            </a>
+          </li>
           <li class="nav-item">
             <a href="<?= base_url('tech/activity/index') ?>" class="nav-link">
               <i class="nav-icon fas fa-paper-plane"></i>
