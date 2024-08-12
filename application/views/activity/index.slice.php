0b7c7edff8e03cfe1916b4507a3ac41d213f56d2
syauqyitho <syauqy.devithogny@gmail.com>
2024-08-12 15:02:06 +0700

    refactor(user): clean sidebar and feature list activities

--- application/views/activity/index.slice.php
+++ application/views/activity/index.slice.php
@@ -9,7 +9,7 @@
                 <!-- <h3 class="card-title"></h3> -->
                 <a href="<?= base_url('user/activity/add') ?>" class="card-tools btn btn-primary"><i class="fas fa-plus mx-1"></i>Transaksi Jasa</a>
 
-                <div class="">
+                <!-- <div class="">
                   <div class="input-group input-group-sm" style="width: 150px;">
                     <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
 
@@ -19,33 +19,19 @@
                       </button>
                     </div>
                   </div>
-                </div>
+                </div> -->
               </div>
               <!-- /.card-header -->
               <div class="card-body table-responsive p-0" >
                 <table class="table table-head-fixed text-nowrap">
-                  <!-- <thead>
-                    <tr>
-                      <th>No</th>
-                      <th>User</th>
-                      <th>Status</th>
-                      <th>Date</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead> -->
                   <tbody>
-                    <!-- <tr>
-                      <td>982</td>
-                      <td>Rocky Doe</td>
-                      <td>11-7-2014</td>
-                      <td><span class="tag tag-danger">Denied</span></td>
-                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                    </tr> -->
                     <?php $no=1; foreach ($activities as $act) : ?>
                       <tr>
-                        <td class="d-flex flex-column">
-                          <p><?= $act->constrain ?></p>
-                          <p><?= $act->created_at ?></p>
+                        <td class="col">
+                          <div class="d-flex flex-column">
+                            <p><?= $act->constrain ?></p>
+                            <p><?= $act->created_at ?></p>
+                          </div>
                         </td>
                         <td class="align-middle"><?= $act->status ?></td>
                         <td class="align-middle">
